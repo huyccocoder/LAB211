6d74d85e6de8d5d1e79875e7790232ad159a7b9d
huyccocoder <hhcc782005@gmail.com>
2024-12-27 23:23:11 +0700

    15%

---
+++ J1.S.P0070/src/view/Display.java
@@ -0,0 +1,8 @@
+public class Display {
+    public void display(){
+        System.out.println("---------- Login Program ----------");
+        System.out.println("1. Vietnamese");
+        System.out.println("2. English");
+        System.out.println("3. Exit");
+    }
+}
