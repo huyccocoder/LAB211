7e7da4dabcace9dd95498667c7026d3a31eec982
huyccocoder <hhcc782005@gmail.com>
2025-01-16 21:15:40 +0700

    start

--- J1.L.P0022/src/view/Display.java
+++ J1.L.P0022/src/view/Display.java
@@ -9,5 +9,12 @@ package view;
  * @author Admin
  */
 public class Display {
-    
+    public void displayCandidate(){
+        System.out.println("CANDIDATE MANAGEMENT SYSTEM");
+        System.out.println("\t1. Experience");
+        System.out.println("\t2. Fresher");
+        System.out.println("\t3. Internship");
+        System.out.println("\t4. Searching");
+        System.out.println("\t5. Exit");
+    }
 }
--- J1.L.P0022/src/view/main.java
+++ J1.L.P0022/src/view/main.java
@@ -4,10 +4,31 @@
  */
 package view;
 
+import ultis.Validation;
+
 /**
  *
  * @author Admin
  */
 public class main {
-    
+    public static void main(String[] args) {
+        Display display = new Display();
+        Validation check = new Validation();
+        
+        display.displayCandidate();
+        
+        int choice = check.inputInt("Your choice: ", 1, 5);
+        switch (choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5: 
+                System.exit(0);
+        }
+    }
 }
